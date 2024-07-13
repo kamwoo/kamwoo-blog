59ab9ce15a499ed5af7b70f0ffdbfb3bac956b3e
kamwoo <rkadndud06@naver.com>
2024-07-13 13:11:50 +0900

    feat: og tag hover

--- src/components/post/mdx-components/og-box.tsx
+++ src/components/post/mdx-components/og-box.tsx
@@ -30,8 +30,8 @@ export const OGBox = ({ data, href }: Props) => {
   return og && og.title ? (
     <a
       href={og.url}
-      className='pointer w-full h-16 md:h-28 flex justify-between bg-slate-900 py-0 no-underline rounded-sm my-4'>
-      <span className='flex flex-col h-full py-3 px-4 space-y-2 overflow-hidden'>
+      className='pointer w-full h-16 md:h-28 flex justify-between bg-slate-900 py-0 no-underline rounded-sm my-4 hover:bg-slate-950'>
+      <span className='flex flex-col h-full py-2 px-3 md:py-3 md:px-4 space-y-2 overflow-hidden'>
         <span className='text-slate-200 text-xs md:text-base font-semibold'>{og.title}</span>
         <span className='text-slate-200 text-xs md:text-sm overflow-hidden text-ellipsis whitespace-nowrap'>
           {og.description}
